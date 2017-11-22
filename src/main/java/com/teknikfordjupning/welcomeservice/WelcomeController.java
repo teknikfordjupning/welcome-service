df43bc9358f8d042e57f017c378c976a78322104
Thomas Hansson <thha@intra.abalon.se>
2017-11-22 14:43:27 +0100

    First Commit

---
+++ src/main/java/com/teknikfordjupning/welcomeservice/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.teknikfordjupning.welcomeservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Simon!";
+    }
+}
---
+++ src/main/java/com/teknikfordjupning/welcomeservice/WelcomeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.teknikfordjupning.welcomeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeServiceApplication.class, args);
+	}
+}
